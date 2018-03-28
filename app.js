a9541ad38e58f1a177ac528180f6a03135025c8e
Amrish Jhaveri <ajhave5@uic.edu>
2018-03-27 20:33:41 -0500

    extracting the requried data

--- app.js
+++ app.js
@@ -29,11 +29,22 @@ octokit.search.repos(getParams(1)).then(result => {
     console.log('error', JSON.stringify(reject));
 });
 
+//read the file contents
 var repoDetails=JSON.parse(fs.readFileSync(CONSTANTS.REPOS_DATA_FILE));
-console.log('in Result',JSON.stringify(repoDetails,undefined,2));
+//console.log('in Result',JSON.stringify(repoDetails,undefined,2));
 
+//iterate over the data and store the required info from each repo metadata.
 repoDetails.data.items.forEach(element => {
-    reqData.addData(element);
+    var data={
+        id:element.id,
+        name: element.name,
+        issues_url: element.issues_url,
+        pulls_url:element.pulls_url,
+        created_at:element.created_at,
+        has_issues:element.has_issues
+    };
+    reqData.addData(data);
 });
 
+fs.writeFileSync('requiredData.json',JSON.stringify(reqData.getData(),undefined,2));
 console.log(reqData.getData().length);
