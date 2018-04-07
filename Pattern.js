1ea07d116f39f51a50d833ebc4b0bb9b0071aca3
Amrish Jhaveri <ajhave5@uic.edu>
2018-04-07 17:30:46 -0500

    pattern module

--- Pattern.js
+++ Pattern.js
@@ -1,3 +1,5 @@
+const AnalyzerObj=require('./AnalyzerObj');
+
 var CONSTANTS = {
     ADD_IMPORT: '[+][ ]*import ',
     REMOVE_IMPORT: '[-][ ]*import '
@@ -8,14 +10,18 @@ var PATTERN_ID = {
     REMOVE_IMPORT: 'REMOVE_IMPORT'
 }
 
-async function pattermAddImport(content) {
+async function pattermAddImport(change) {
     let patternRegex = new RegExp(CONSTANTS.ADD_IMPORT);
-    patternRegex.test(content) ? getAnalyzerObj() : undefined;
+    patternRegex.test(change.content) ? getAnalyzerObj(change) : undefined;
 }
 
-async function pattermRemoveImport(content) {
+async function pattermRemoveImport(change) {
     let patternRegex = new RegExp(CONSTANTS.REMOVE_IMPORT);
-    patternRegex.test(content) ? getAnalyzerObj() : undefined;
+    patternRegex.test(change.content) ? getAnalyzerObj(change) : undefined;
+}
+
+function getAnalyzerObj(change){
+    let reultObj= new AnalyzerObj();
 }
 
 module.exports.Pattern = {
