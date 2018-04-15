59a787ae617365fec46956b6d686cccd48b9f02d
Chinmay Gangal <chinmay2312@gmail.com>
2018-04-14 21:24:26 -0500

    Understand

---
+++ jsonRead.py
@@ -0,0 +1,22 @@
+import json
+import understandscript as undScr
+import os
+
+repoData = json.load(open('RepoData.json'))
+repoName = repoData["iluwatar/java-design-patterns"]['name']
+
+changeData = json.load(open('RequiredData.json'))
+
+matches = changeData["REMOVE_IMPORT"]["matches"]
+
+for match in matches:
+	change = match["change"]
+	relPath = repoName + "/" + match['file']
+	fileName = relPath.split("/")[-1]
+	fName = fileName.split(".")[0]
+	targetEnt = match["change"]["findImpactFor"]
+	
+	op = undScr.getImpacts(relPath,targetEnt,fileName)
+	#op = os.system("python .\understandscript.py {filePath} {targEnt} {fileName}".format(filePath=relPath,targEnt=targetEnt, fileName=fileName))
+	if op:
+		print(targetEnt,"\t",op)
---
+++ understandscript.py
@@ -0,0 +1,74 @@
+#Input: entity name
+#Output: List of impacted entities
+
+#arguments
+#1: relative path of file to be analyzed
+#2: entity to find impact for
+#3: filename without extension
+ 
+#from sys import argv
+import understand
+import subprocess
+
+def getImpacts(a,b,c):
+	argv=[1,2,3,4]
+	argv[1] =a
+	argv[2] = b
+	argv[3] =c
+	#udb_path = '\\Project2'
+	#language = 'java'
+	project_root = '\\javaee7-sample-master'
+	subprocess.check_output("und create -db Temp -languages java",shell=True)
+	subprocess.check_output("und add -db Temp {project}".format(project=argv[1]), shell=True)
+	subprocess.check_output("und analyze Temp.udb",shell=True)
+	#print("und add -db Temp {project}".format(project=argv[1]))
+	#db = understand.open(".\\Project2\\CalcTest.udb")
+	db = understand.open("Temp.udb")
+
+	lineNums=[]
+	#print('hey')
+	fileEnts = []
+	allEnts = db.ents()
+	for ent in allEnts:
+		#print()
+		#if(ent.uniquename().startswith('jtestUnd')):
+		#fileEnts.append(ent.uniquename())
+		#if(ent.uniquename() == "jtestUnd.Calculator2.main.a"):
+		#print(ent.name())
+		#print(argv[2])
+		if ent.name()==argv[2] and argv[3] in ent.uniquename():
+			#print("Entity: ",ent.uniquename())
+			#print("Type: ",ent.kind())
+			if ent.refs():
+				#print("\nReferences:")
+				for dep in ent.refs():
+					#print()
+					#print("Kind:",dep.kind())
+					#print("Name:",dep.ent())
+					#print("Line:",dep.line())
+					if dep.kind().__str__()=='Use':
+						#print('Line added: ',dep.line())
+						lineNums.append(dep.line())
+			#break
+
+	print()
+	#for e in fileEnts:
+	#	print(e)
+
+	impactList = []
+	for ent in db.ents():
+		#if(ent.uniquename().startswith('jtestUnd.Calculator2')):
+		#print("Entity: ",ent.uniquename())
+		if argv[3] not in ent.uniquename():
+			continue
+		for dep in ent.refs():
+			if dep.kind().__str__()in ['Set','Define', 'Call'] and dep.line() in lineNums:
+				#print(dep.kind(), "\t", dep.ent())
+				#print(dep.ent())
+				impact = dep.kind(),"\t",dep.ent()
+				impactList.append(impact)
+
+	#subprocess.check_output("pwd", shell=True)			
+	#subprocess.check_output("rm Temp.udb", shell=True)			
+	#db.close()
+	return impactList
