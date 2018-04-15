fbd2a82c36a20fc259844c373e8e66e3ec7f1220
Chinmay Gangal <cganga2@uic.edu>
2018-04-15 02:57:03 +0000

    understandscript.py edited online with Bitbucket

--- understandscript.py
+++ understandscript.py
@@ -11,15 +11,22 @@ import understand
 import subprocess
 
 def getImpacts(a,b,c):
+	#initialization of argv list of arguments
 	argv=[1,2,3,4]
-	argv[1] =a
-	argv[2] = b
-	argv[3] =c
+	argv[1] =a	#relative path of file to be analyzed
+	argv[2] = b	#entity to find impact for
+	argv[3] =c	#filename without extension
 	#udb_path = '\\Project2'
 	#language = 'java'
 	project_root = '\\javaee7-sample-master'
+	
+	#create new Understand project for file to be analyzed
 	subprocess.check_output("und create -db Temp -languages java",shell=True)
+	
+	#add desired file to project
 	subprocess.check_output("und add -db Temp {project}".format(project=argv[1]), shell=True)
+	
+	#perform Understand analysis
 	subprocess.check_output("und analyze Temp.udb",shell=True)
 	#print("und add -db Temp {project}".format(project=argv[1]))
 	#db = understand.open(".\\Project2\\CalcTest.udb")
@@ -39,15 +46,23 @@ def getImpacts(a,b,c):
 		if ent.name()==argv[2] and argv[3] in ent.uniquename():
 			#print("Entity: ",ent.uniquename())
 			#print("Type: ",ent.kind())
+			
+			#if entity has references
 			if ent.refs():
 				#print("\nReferences:")
+				
+				#iterate over dependent entities in reference of entity-in-focus
 				for dep in ent.refs():
 					#print()
 					#print("Kind:",dep.kind())
 					#print("Name:",dep.ent())
 					#print("Line:",dep.line())
+					
+					#verify if this reference is the target entity
 					if dep.kind().__str__()=='Use':
 						#print('Line added: ',dep.line())
+						
+						#store line numbers, for later reference
 						lineNums.append(dep.line())
 			#break
 
@@ -55,6 +70,7 @@ def getImpacts(a,b,c):
 	#for e in fileEnts:
 	#	print(e)
 
+	#initialize empty list of impacted entities
 	impactList = []
 	for ent in db.ents():
 		#if(ent.uniquename().startswith('jtestUnd.Calculator2')):
