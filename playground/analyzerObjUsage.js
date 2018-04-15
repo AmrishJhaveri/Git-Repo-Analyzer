c605d5ac32124826ff54fff1ef156b282bdc80e2
Amrish Jhaveri <ajhave5@uic.edu>
2018-04-14 20:53:02 -0500

    redundant files

--- playground/analyzerObjUsage.js
+++
@@ -1,5 +0,0 @@
-const AnalyzerObj=require('./../AnalyzerObj');
-
-let obj = new AnalyzerObj('fileName.js');
-obj.addConstruct({factor:'parameterChange',oldParameter:'int'});
-console.log(JSON.stringify(obj));
